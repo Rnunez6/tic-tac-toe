f45161294a260aaeb2088e174263e4550065a33a
RomanNunez <roman_nunez@ymail.com>
2021-03-29 09:35:27 -0500

    Completed Tic Tac Toe App

--- src/index.js
+++ src/index.js
@@ -47,9 +47,16 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
+      // the rest has not changed
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
@@ -70,6 +77,8 @@ class Board extends React.Component {
       </div>
     );
   }
+
+  
 }
  
 class Game extends React.Component {
@@ -88,6 +97,25 @@ class Game extends React.Component {
   }
 }
  
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
  
 ReactDOM.render(
